9abe924901919f7ca35a944fa80595290dbd8e6d
denerval1111 <denerval1111@gmail.com>
2025-06-20 09:16:59 -0300

    Update BlogClient.tsx

--- app/blog/BlogClient.tsx
+++ app/blog/BlogClient.tsx
@@ -30,7 +30,7 @@ const BlogClient: React.FC<BlogClientProps> = ({ initialPosts = [] }) => {
       date: '2024-06-10',
       author: 'Dr. Denerval',
       category: 'Saúde Mental',
-      image: 'https://images.unsplash.com/photo-1559757148-5c350d0d3c56?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80',
+      image: 'https://images/artigo estresse.png',
       readTime: '9 min de leitura',
       tags: ['ansiedade', 'estresse', 'saúde mental', 'mindfulness', 'respiração']
     },
