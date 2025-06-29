92c271dad9305b7d25ba7dbac78262906bd06cc5
denerval1111 <denerval1111@gmail.com>
2025-06-29 10:25:47 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -99,7 +99,7 @@ export default function Footer() {
             Contato
           </h4>
           <p style={{ color: '#CCC', marginBottom: '0.5rem' }}>
-            <strong>Email:</strong> contato@desafiovitalidade.com.br
+            <strong>Email:</strong> contato@desafiovitalidade.com.br</Link>
           </p>
           <p style={{ color: '#CCC', marginBottom: '0.5rem' }}>
             <strong>Telefone:</strong> (18) 99817-9849
